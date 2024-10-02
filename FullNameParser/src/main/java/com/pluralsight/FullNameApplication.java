58e22cbe77044911a435232f63e34857d6d77ae0
Djharness <djharness2@gmail.com>
2024-10-02 13:41:27 -0400

    Full Name App

---
+++ FullNameParser/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+import java.util.*;
+public class FullNameApplication {
+    static Scanner scanner = new Scanner(System.in);
+
+    String firstName = PromptForString("First Name: ");
+    String middleName = PromptForString("Middle Name");
+    String lastName = PromptForString("Middle name");
+    String suffix = PromptForString("Suffix: ");
+
+
+
+
+
+
+
+
+
+    public static String PromptForString(String prompt){
+        System
+    }
+
+}
+
+
+
