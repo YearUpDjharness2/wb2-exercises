463d00a4d610c55150f86f821aa69d872628ebcc
Djharness <djharness2@gmail.com>
2024-10-04 14:24:50 -0400

    Looping

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+
+             do {
+                System.out.println("I <3 Java");
+                count++;
+            }while (count < 5);
+        }
+    }
+
